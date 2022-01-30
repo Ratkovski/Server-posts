63ff72ad44933e29ac8eaddec6af37969955c4b9
Ratkovski <drezcry@hotmail.com>
2022-01-30 13:35:52 -0300

    chore: Codebase

---
+++ src/main/java/br/com/academy/ratkovski/server/posts/Server/Posts/ServerPostsApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.server.posts.Server.Posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academy/ratkovski/server/posts/Server/Posts/ServerPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.server.posts.Server.Posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
